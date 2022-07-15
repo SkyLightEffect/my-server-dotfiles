946f099c52a453cd7894b056c613c1ca978ea009
SkyLightEffect <71511518+SkyLightEffect@users.noreply.github.com>
2022-07-15 14:46:50 +0000

    onedark link fix 2

--- scripts/links.sh
+++ scripts/links.sh
@@ -3,4 +3,8 @@ ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 #ln -s ~/.dotfiles/.vim/ ~/.vim/
-ln -sd ~/.dotfiles/.vim/ ~/
+
+mkdir -p ~/.vim/colors
+mkdir -p ~/.vim/autoload
+ln -s ~/.dotfiles/.vim/colors/onedark.vim ~/.vim/colors/onedark.vim
+ln -s ~/.dotfiles/.vim/autoload/onedark.vim ~/.vim/autoload/onedark.vim
