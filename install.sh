378f42a0a52f5743c440e8d3085a4cd1f5147d94
SkyLightEffect <71511518+SkyLightEffect@users.noreply.github.com>
2022-07-15 17:36:30 +0000

    Fix install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ DOT=~/.dotfiles
 SCRIPTS=$DOT/scripts
 TMP=$DOT/.temp
 chmod -R u+x $SCRIPTS
-sh $SCRIPTS/install-packages.sh $SCRIPTS
+sh $SCRIPTS/install-packages.sh $SCRIPTS $TMP
 sh $SCRIPTS/vim-onedark-installer.sh
 sh $SCRIPTS/links.sh
 sh $SCRIPTS/zsh-installer.sh
