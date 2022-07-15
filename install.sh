3f15779592a6a160c221619ce7776aa41a0861c4
SkyLightEffect <71511518+SkyLightEffect@users.noreply.github.com>
2022-07-15 17:09:37 +0000

    fixed install script 3

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ DOT=~/.dotfiles
 SCRIPTS=$DOT/scripts
 chmod -R u+x $SCRIPTS
 sh $SCRIPTS/install-packages.sh $SCRIPTS
-sh $SCRIPTS/scripts/vim-onedark-installer.sh
-sh $SCRIPTS/scripts/links.sh
-sh $SCRIPTS/scripts/zsh-installer.sh
+sh $SCRIPTS/vim-onedark-installer.sh
+sh $SCRIPTS/links.sh
+sh $SCRIPTS/zsh-installer.sh
 zsh
